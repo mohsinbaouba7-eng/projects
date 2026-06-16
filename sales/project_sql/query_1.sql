43627a027961c8006fed830158938361b3232cb3
mohsinbaouba7-eng <mohsinbaouba7@gmail.com>
2026-06-16 12:13:48 +0000

    add YoY montly sales growth query

--- sales/project_sql/query_1.sql
+++ sales/project_sql/query_1.sql
@@ -57,7 +57,7 @@ SELECT
 FROM 
 sales_table;
 
-/* II- AFRER RUNNED THE PREVIOUS QUERY YOY CAN NOTICE THAT 
+/*  AFRER RUNNED THE PREVIOUS QUERY YOY CAN NOTICE THAT 
 THE NUMBERS APPEARING AS A RESULT ARE NOT PROPERLY READ
 SO WE ARE GOING TO IMPROVE A BIT READABILITY BY ADDING
 1- DOLLARS SIGN
@@ -75,7 +75,9 @@ SELECT
 FROM 
 sales_table;
 
--- III - MOST PROFITABLE ITEM_TYPES
+
+-- II - MOST PROFITABLE ITEM_TYPES
+
 
 SELECT
     sales_table."Item_Type",
@@ -87,8 +89,10 @@ GROUP BY
     sales_table."Item_Type"
 ORDER BY Total_Profit DESC;
 
-/* IV -  CHECKING PERFORMANCE BY REGION TO SEE WHERE OUR
-TARGET MARKETS ARE 
+/* III -  CHECKING PERFORMANCE BY REGION :
+--> WHAT REGION HAS THE HIGHEST PRPFIT MARGIN, AND 
+WHAT REGION HAS THE HIGHEST REVENUE BUT LOW PROFIT 
+MARCHINS MARGINS ?
 */
 
 SELECT 
@@ -98,4 +102,61 @@ SELECT
 FROM sales_table
 GROUP BY sales_table."Region"
 ORDER BY PROFIT_MARGIN DESC;
+
+
+
+/* 
+    IV- YEAR - TO - YEAR GROWTH :
+ IS THE COMPANY GROWING OVER ITS PROFIT OVER OVER YEARS, OR DROPPING ?
+    1- CREATE 2 NEW COLUMNS "Sales_year , Sales_month"
+    2- EXTRACT MONTH/YEAR/DAY 
+    3- YEAR SALES AND MONTH SALES 
+
+*/
+
+ALTER TABLE sales_table
+    ADD COLUMN sales_year INT,
+    ADD COLUMN sales_Month INT;
+
+UPDATE sales_table
+SET 
+        sales_year =  EXTRACT(YEAR FROM "Order_Date"),
+        sales_month = EXTRACT(MONTH FROM "Order_Date");
+
+
+-- YOY Sales 
+
+WITH MonthlySales AS (
+    SELECT 
+        sales_year,
+        sales_month,
+        SUM("Total_Revenue") AS current_sales
+    FROM sales_table
+    GROUP BY sales_year, sales_month
+)
+SELECT 
+    sales_year,
+    sales_month,
+    -- 1. Format current sales nicely for presentation
+    TO_CHAR(current_sales, '$999,999,999,999') AS current_year_sales,
+    
+    -- 2. Grab the sales from exactly 12 months ago
+    LAG(current_sales, 12) OVER(ORDER BY sales_year, sales_month) AS previous_year_sales,
+    
+    -- 3. Calculate YoY Growth Percentage
+    ROUND(
+        ((current_sales - LAG(current_sales, 12) OVER(ORDER BY sales_year, sales_month)) 
+        / LAG(current_sales, 12) OVER(ORDER BY sales_year, sales_month)) * 100, 
+        2
+    ) || '%' AS yoy_growth_percentage
+
+FROM MonthlySales
+ORDER BY sales_year DESC, sales_month DESC;
+
+
+
+
+SELECT *
+    FROM 
+        sales_table;
         
